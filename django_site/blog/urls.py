fb800f0a3ff04fecc8db3594637010662d851376
Gofla <gofla95@list.ru>
2020-01-22 20:16:39 +0300

    made admin part

--- django_site/blog/urls.py
+++ django_site/blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('<slug>', views.article_detail, name='detail'),
-    #re_path('(?P<slug>[\w-]+)/$', views.article_detail, name='detail'),
+    path('create/', views.article_create, name='create'),
     path('', views.article_list, name='list'),
+    path('<slug>', views.article_detail, name='detail'),
 ]
--- django_site/django_site/urls.py
+++ django_site/django_site/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('index/', views.index),
+    path('accounts/', include('accounts.urls')),
     path('', include('blog.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
@@ -13,3 +15,4 @@ urlpatterns = [
 
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
